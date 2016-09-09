c2e23e284aff02153aae35d66f8e3096b45b8fdc
Thomas Bereknyei <tomberek@gmail.com>
2016-09-08 22:35:51 -0400

    Repackage to local

--- default.nix
+++ default.nix
@@ -1,15 +1,18 @@
 { mkDerivation, aeson, array, base, base64-bytestring, binary
-, blaze-html, blaze-markup, bytestring, cmark, containers
+, blaze-html, blaze-markup, bytestring, Cabal, cmark, containers
 , data-default, deepseq, directory, extensible-exceptions
-, filemanip, filepath, ghc-prim, hslua, HTTP, mtl, network
-, network-uri, old-time, pandoc-types, parsec, process, random, SHA
-, stdenv, syb, tagsoup, temporary, text, time, unordered-containers
-, vector, xml, yaml, zip-archive, zlib
+, filemanip, filepath, ghc-prim, ghcjs-base, ghcjs-dom, ghcjs-prim
+, hslua, HTTP, mtl, network, network-uri, old-time, pandoc-types
+, parsec, process, random, SHA, stdenv, syb, tagsoup, temporary
+, text, time, transformers, unordered-containers, vector, xml, yaml
+, zip-archive, zlib
 }:
 mkDerivation {
   pname = "pandoc-light";
   version = "1.17.2";
   src = ./.;
+  isLibrary = true;
+  isExecutable = true;
   libraryHaskellDepends = [
     aeson array base base64-bytestring binary blaze-html blaze-markup
     bytestring cmark containers data-default deepseq directory
@@ -17,6 +20,10 @@ mkDerivation {
     network network-uri old-time pandoc-types parsec process random SHA
     syb tagsoup temporary text time unordered-containers vector xml
     yaml zip-archive zlib
+  ];
+  executableHaskellDepends = [
+    base blaze-html Cabal ghcjs-base ghcjs-dom ghcjs-prim text
+    transformers
   ];
   homepage = "http://pandoc.org";
   description = "Conversion between markup formats";
---
+++ pandoc-editor.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PackageImports #-}
+{-# LANGUAGE JavaScriptFFI      #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE PartialTypeSignatures      #-}
+
+module Main where
+import Text.Pandoc
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Trans.Class (lift)
+-- import Control.Monad (void)
+import Data.Text
+import Text.Blaze.Html.Renderer.String
+import GHCJS.Foreign.Callback
+import GHCJS.DOM.Document (load,getElementById)
+import GHCJS.DOM.Element (keyUp,setInnerHTML)
+import GHCJS.DOM.EventM (on)
+import GHCJS.DOM (currentDocument,currentWindow)
+import GHCJS.Types (JSString,JSVal)
+import Data.JSString (pack)
+import GHCJS.Prim
+import Control.Concurrent
+
+main :: IO ()
+main = do
+    callback <- syncCallback1 ContinueAsync converter_
+    set_callback callback
+    threadDelay $ 200 * 1000
+    Just w <- currentWindow
+    Just d <- currentDocument
+    Just edit <- getElementById d ("edit" :: String)
+    content <- getCodeMirrorContent
+    converter_ content
+    edit `on` keyUp $ liftIO $ do
+        content <- getCodeMirrorContent
+        converter_ content
+    return ()
+
+
+foreign import javascript unsafe "window.editor.getValue()"
+  getCodeMirrorContent :: IO JSVal
+
+{-
+wrapCallback c = do
+  cb <- syncCallback2 AlwaysRetain False $ \this x -> do
+    (Element res) <- runReaderT (c ^. render) this
+    setProp ("result" :: JSString) res x
+  w <- reactWrapCallback cb
+  return (w, cb)
+-}
+
+converter_ :: JSVal -> IO ()
+converter_ new = do
+    case converter (Data.Text.pack $ GHCJS.Prim.fromJSString new) "test" of
+      Just res -> do
+          Just d <- currentDocument
+          Just preview <- getElementById d ("preview" :: String)
+          setInnerHTML preview (Just res :: Maybe String)
+          set_return $ Data.JSString.pack res
+
+      Nothing -> return ()
+
+foreign import javascript unsafe "js_callback_ = $1"
+    set_callback :: Callback a -> IO ()
+foreign import javascript unsafe "js_ret_ = $1"
+    set_return :: JSString -> IO ()
+
+converter :: Text -> String -> Maybe String
+converter input _ = do
+    p <- case readMarkdown def (Data.Text.unpack input) of
+        Left _ -> Nothing
+        Right parsed -> Just parsed
+    Just $ renderHtml $ writeHtml def p
+--}
+
